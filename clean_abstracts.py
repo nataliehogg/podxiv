474e8083d1347f67715eb3f073aacff9fa97984f
nataliehogg <mail@nataliebhogg.com>
2023-02-12 21:20:39 +0100

    switch to pydetex

--- clean_abstracts.py
+++ clean_abstracts.py
@@ -1,18 +1,16 @@
+'''
+we use pydetex to change the latex abstracts to plain text suitable for text-to-speech parsing.
+'''
 import numpy as np
-import re
+import pydetex.pipelines as pyp
 
 dirpath = '/home/natalie/Documents/Projects/podxiv/'
 textpath = 'sample_abstract.txt'
 
 with open (dirpath+textpath, "r") as file:
-    abstract = file.read().replace('\n', '')
+    text = file.read().replace('\n', ' ')
 
-def clean_latex(text):
-    no_dollars = re.sub('\$|\\|', ' ', text)
-    no_backslashes = re.sub('\\\\', '', no_dollars)
-    return no_backslashes
-
-clean_abstract = clean_latex(abstract)
+clean_abstract = pyp.simple(text)
 
 with open(dirpath+'clean_sample_abstract.txt', 'w') as text_file:
     text_file.write(clean_abstract)
